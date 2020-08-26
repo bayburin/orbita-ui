0ed4c744fe6af960e6837a1d2798e3d261ee0da6
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-08-26 15:42:04 +0700

    Добавлен angular material-design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SharedModule } from './shared/shared.module';
 import { ClaimModule } from './modules/claim/claim.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AppComponent } from './app.component';
     CoreModule,
     SharedModule,
     ClaimModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
