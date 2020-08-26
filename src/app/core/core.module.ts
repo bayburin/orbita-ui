365dc36b67dddf4cc1c7a0805407b666988ea7ff
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-08-26 10:03:33 +0700

    Добавлена проверка, что CoreModule загружен только один раз.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 
 @NgModule({})
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule уже загружен. Он должен быть импортирован только в AppModule');
+    }
+  }
+}
