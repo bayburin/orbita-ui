c3955b6506e2b50427a9adc65f05856ed5242c48
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-09-03 11:39:22 +0700

    auth-lib: Добавлен класс RequestState для генерации кода авторизации.

---
+++ projects/auth-center/src/lib/models/request_state.model.ts
@@ -0,0 +1,12 @@
+export class RequestState {
+  static generateCode(limit: number = 30): string {
+    let result = '';
+    const dictionary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+      result += dictionary.charAt(Math.floor(Math.random() * dictionary.length));
+    }
+
+    return result;
+  }
+}
