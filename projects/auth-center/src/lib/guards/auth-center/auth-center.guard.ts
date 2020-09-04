ad3bfb3433c49b92c6b4151d7a481d4f48012591
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-09-04 11:57:19 +0700

    auth-lib: Класс AuthCenterGuard перенесен в поддиректорию auth-center.

--- projects/auth-center/src/lib/guards/auth-center/auth-center.guard.ts
+++ projects/auth-center/src/lib/guards/auth-center/auth-center.guard.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthCenterGuard implements CanActivate {
-  constructor(private store: Store<State>) { }
+  constructor() { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.store.select(AuthSelectors.getIsAuthenticateed);
+    return true;
   }
 }
