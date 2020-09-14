0faf74c11f6112520e60dc4659e23e6d9c870f0a
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-09-14 15:52:32 +0700

    auth-lib: Класс CurrentUser добавлен в public-api.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 
-import { AuthHelper } from 'auth-center';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private authHelper: AuthHelper) {
-    console.log(authHelper.getCurrentUser());
-  }
+  constructor() { }
 }
