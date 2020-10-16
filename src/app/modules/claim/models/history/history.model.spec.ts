d4402d90721c6c6229a0e5eb272f9010c83011d1
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-10-16 11:24:11 +0700

    Добавлены тесты на класс History.

---
+++ src/app/modules/claim/models/history/history.model.spec.ts
@@ -0,0 +1,24 @@
+import { IHistory } from '@modules/claim/interfaces/history.interface';
+import { IHistoryBuilder } from '@modules/claim/builders/i-history.builder';
+import { History } from '@modules/claim/models/history/history.model';
+
+describe('History', () => {
+  let iHistory: IHistory;
+
+  beforeEach(() => {
+    iHistory = new IHistoryBuilder().build();
+  });
+
+  describe('Constructor', () => {
+    it('should create instance of History', () => {
+      expect(new History(iHistory)).toBeTruthy();
+    });
+
+    it('should accept values', () => {
+      const runtime = new History(iHistory);
+
+      expect(runtime.id).toEqual(iHistory.id);
+      expect(runtime.workId).toEqual(iHistory.work_id);
+    });
+  });
+});
