446edd000c6a6f31982327186e051418dff7bee9
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2020-12-30 12:12:45 +0700

    Добавлен вывод заполненной формы в PreviewNewSdRequestComponent компонент.

--- src/app/modules/sd-request/components/preview-new-sd-request/preview-new-sd-request.component.ts
+++ src/app/modules/sd-request/components/preview-new-sd-request/preview-new-sd-request.component.ts
@@ -1,5 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup } from '@angular/forms';
+
+import { claimPrioritiesMap } from '@modules/claim/enums/claim-priorities.enum';
+import { UserFacade } from '@modules/user/facades/user.facade';
+import { IUserGroup } from '@modules/user/interfaces/user-group.interface';
 
 @Component({
   selector: 'app-preview-new-sd-request',
@@ -7,12 +12,22 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./preview-new-sd-request.component.scss']
 })
 export class PreviewNewSdRequestComponent implements OnInit {
+  priorities = claimPrioritiesMap;
   form = this.data.form;
+  userGroups: IUserGroup[];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { form: any }) {}
+  get sourceSnapshot() {
+    return this.form.source_snapshot;
+  }
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { form: any },
+    private userFacade: UserFacade
+  ) {
+    this.userGroups = this.userFacade.createGroups(this.form.users);
+  }
 
   ngOnInit(): void {
     console.log(this.data);
   }
-
 }
