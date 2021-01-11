8799e91207794aac06762a891dd0acba42af505c
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-01-11 09:21:01 +0700

    Подправлены тесты для PreviewNewSdRequestComponent.

--- src/app/modules/sd-request/components/preview-new-sd-request/preview-new-sd-request.component.spec.ts
+++ src/app/modules/sd-request/components/preview-new-sd-request/preview-new-sd-request.component.spec.ts
@@ -4,10 +4,14 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { PreviewNewSdRequestComponent } from './preview-new-sd-request.component';
 import { MaterialModule } from '@shared/material.module';
+import { UserFacade } from '@modules/user/facades/user.facade';
+import { UserFacadeStub } from '@modules/user/facades/user.facade.stub';
+import { DatetimePipe } from '@shared/pipes/datetime/datetime.pipe';
 
 describe('PreviewNewSdRequestComponent', () => {
   let component: PreviewNewSdRequestComponent;
   let fixture: ComponentFixture<PreviewNewSdRequestComponent>;
+  const form = { attrs: { }, source_snapshot: { }, attachments: [] };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,8 +19,11 @@ describe('PreviewNewSdRequestComponent', () => {
         MaterialModule,
         HttpClientTestingModule
       ],
-      providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }],
-      declarations: [PreviewNewSdRequestComponent]
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: { form } },
+        { provide: UserFacade, useClass: UserFacadeStub }
+      ],
+      declarations: [PreviewNewSdRequestComponent, DatetimePipe]
     })
     .compileComponents();
   }));
